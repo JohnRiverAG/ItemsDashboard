611c7290063cd97edd4259c2bc0b6c10d62f0ec2
John Arango <johnunivalle@gmail.com>
2025-07-23 02:33:04 +0200

    Creación proyecto con Spring

---
+++ src/main/java/com/spring/itemsdash/ItemsdashApplication.java
@@ -0,0 +1,13 @@
+package com.spring.itemsdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsdashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/itemsdash/ItemsdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.itemsdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
